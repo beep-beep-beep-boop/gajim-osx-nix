e3f4f728ad7c3ca2ac0026a9a34ab8bcf5c4f799
beep-beep-beep-boop <79901832+beep-beep-beep-boop@users.noreply.github.com>
2023-07-04 20:36:50 -0600

    launch gajim detached from terminal

--- launch_gajim.fish
+++ launch_gajim.fish
@@ -5,7 +5,7 @@ source env/bin/activate.fish
 
 set dbus_session_conf (path dirname (path dirname (which dbus-run-session)))/share/dbus-1/session.conf
 
-dbus-run-session --config-file=$dbus_session_conf python3 gajim/launch.py
+dtach -n /tmp/gajim-session dbus-run-session --config-file=$dbus_session_conf python3 gajim/launch.py
 
 #nohup ./gajim/launch.py &
 #dtach ./gajim/launch.py
